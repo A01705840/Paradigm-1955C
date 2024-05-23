9433cb7f3fb17f4917a67268be11ac2581fad330
Maria José Soto <A01705840@tec.mx>
2024-05-23 08:24:52 -0600

    Añadir programación paralelo con OpenMP

--- 1955c.cpp
+++ 1955c.cpp
@@ -1,60 +1,114 @@
 #include <iostream>
-#include <vector>
+#include <omp.h>
+#include <algorithm>
+
+#define SIZE 100000000
 using namespace std;
 
+int tests;
+int res;
+
+// Acumular los valores para después ingresar en el array
+void set_values(int start, int n, int val[]){
+    for (int i = start; i < n; i++) {
+        std::cin >> val[i];
+    }
+}
+
+//Se asignan los valores al array
+void set_durability(int start, int n, int* arr, int val[]){
+    for(int i = start; i < n; i++){
+        arr[i] = val[i];
+    }
+}
+
+//Para poder hace el parallel programming, 
+void left_iter(int n, int* arr, int k1, int &local_res){ 
+    for (int j = 0; j < n; ++j) {
+        if(k1 > arr[j]){
+            local_res +=1;
+            k1 -= arr[j];
+            cout << "left attacks " << k1 << "\n";
+        }else if(k1 == arr[j]){
+            local_res +=1;
+            k1 = 0;
+            cout << "left attacks done \n";
+        }else if(k1 < arr[j]){
+            break;
+        }
+    }
+}
+
+void right_iter(int n, int* arr, int k2, int &local_res){
+    for(int j = 1; j < n; ++j){
+        if(k2 > arr[-j]){
+            local_res +=1;
+            k2 -= arr[-j];
+            cout << "right attacks " << k2 << "\n";
+        }else if(k2 == arr[-j]){
+            local_res +=1;
+            k2 = 0;
+            cout << "right attacks done \n";
+        }else if(k2 < arr[-j]){
+            break;
+        }
+    }
+}
+
 int main() {
-    int tests;
-    int res;
+    int *arg1 = new int[SIZE];
     std::cin >> tests;
 
     while (tests != 0) {
         int n, k;
         std::cin >> n >> k;
+        int *arr = new int[n];
 
-        std::vector<int> arr(n);
-        for (int i = 0; i < n; ++i) {
-            std::cin >> arr[i];
-        }
-        //std::cout << k%2 << std::endl;
-        int k1;
-        int k2;
-        if (k%2 == 0) {
-            k2 = k/2;
-            k1 = k/2;
-            //std::cout << "k1: " << k1 << ", k2: " << k2 << std::endl;
-        } else if (k%2 != 0) {
-            k1 = k/2 + 1;
-            k2 = k/2;
-            //std::cout << "k1: " << k1 << ", k2: " << k2 << std::endl;
-        }
-
-        for (int j = 0; j < n; ++j) {
-            if(k1 > arr[j]){
-                res +=1;
-                k1 -= arr[j];
-            }else if(k1 == arr[j]){
-                res +=1;
-                k1 = 0;
-                break;
-            }else if(k1 < arr[j]){
-                break;
+        int val[n];
+        set_values(0, n, val);
+        
+        #pragma omp parallel sections
+        {
+            #pragma omp section
+            {
+                set_durability(0, n/2, arg1, val);
+            }
+            #pragma omp section
+            {
+                set_durability(n/2, n, arg1, val);
             }
         }
-        for(int j = 1; j < n; ++j){
-            if(k2 > arr[-j]){
-                res +=1;
-                k2 -= arr[-j];
-            }else if(k2 == arr[-j]){
-                res +=1;
-                k2 = 0;
-                break;
-            }else if(k2 < arr[-j]){
-                break;
+
+        int k1, k2;
+        if (k % 2 == 0) {
+            k2 = k / 2;
+            k1 = k / 2;
+            cout << k1 << k2;
+        } else {
+            k1 = k / 2 + 1;
+            k2 = k / 2;
+            cout << k1 << k2;
+        }
+
+        int local_res_left = 0;
+        int local_res_right = 0;
+
+        #pragma omp parallel sections
+        {
+            #pragma omp section
+            {
+                left_iter(n, arg1, k1, local_res_left);
+            }
+            #pragma omp section
+            {
+                right_iter(n, arg1, k2, local_res_right);
             }
         }
-        //cout << "res: " << res << endl;
+
+        res = local_res_left + local_res_right;
+
+        cout << "res: " << res << endl;
         tests--;
-        return res;
     }
     return 0;
 }
